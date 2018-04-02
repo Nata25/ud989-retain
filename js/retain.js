53e9a816b0ae16e565e3281249497b9ff28d11be
Natalya Ivanova <natalyas.ivanova@gmail.com>
2018-04-02 16:07:00 +0300

    Add a date to a note

--- js/retain.js
+++ js/retain.js
@@ -20,7 +20,8 @@ $(function(){
     var octopus = {
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                date: Date.now()
             });
             view.render();
         },
@@ -51,9 +52,11 @@ $(function(){
         render: function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
-                        note.content +
-                    '</li>';
+                htmlStr += 
+                '<li class="note">'+
+                    '<span class="note-date">' + new Date(note.date).toString() + '</span>'
+                    + note.content +
+                '</li>';
             });
             this.noteList.html( htmlStr );
         }
